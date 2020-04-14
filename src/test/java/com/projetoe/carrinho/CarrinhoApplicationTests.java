ba7a646b088fd8d9358d6f6986aa60c55b3d66bf
Joao Octavio <joaooctf@gmail.com>
2020-04-14 19:37:10 -0300

    Initial Commit

---
+++ src/test/java/com/projetoe/carrinho/CarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoe.carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
